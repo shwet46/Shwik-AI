ff3ae46d174d76ece0bb039dbf3f03094565e10c
Shweta <beherashweta54@gmail.com>
2025-03-07 10:36:26 +0530

    added hero

--- shwik/src/app/page.tsx
+++ shwik/src/app/page.tsx
@@ -4,7 +4,7 @@ import Hero from "@/components/Hero";
 export default function Home() {
   return (
     <>
-    <main className="min-h-screen bg-white antialiased">
+    <main className="min-h-screen antialiased">
       <Hero/>
     </main>
     </>
--- shwik/src/components/Hero.tsx
+++ shwik/src/components/Hero.tsx
@@ -2,15 +2,23 @@ import React from 'react'
 
 function Hero() {
   return (
-    <div className="h-auto md:h-[40rem] w-full rounded-md flex flex-col items-center justify-center relative overflow-hidden mx-auto py-10 md:py-0">
-        <div>
-            <h2 className="mt-20 md:mt-0 text-4xl md:text-7xl font-bold text-pink-600"></h2>
+    <div className="h-auto w-full rounded-md relative overflow-hidden mx-auto py-10 mt-18 px-4 md:py-16">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-8 items-center max-w-6xl mx-auto">
+        <div className="order-2 md:order-1 flex justify-center md:justify-start">
+          <img src="images/heroimg.png" alt="Hero image" className="w-full max-w-[250px] sm:max-w-[300px] md:max-w-[400px] h-auto mx-auto"  />
         </div>
-        <div>
-            
+        
+        <div className="order-1 md:order-2 flex flex-col text-center md:text-left">
+          <h2 className="text-4xl md:text-6xl lg:text-7xl font-bold text-pink-600 mb-6">Shwik</h2>
+          <h3 className="text-lg md:text-xl font-bold text-indigo-950">
+            Shwik is a professional helper that let's you solve problems related
+            to your everyday problems related to documents. May it be creating a document, 
+            having a document summarized and many more issues we face.
+          </h3>
         </div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Hero
